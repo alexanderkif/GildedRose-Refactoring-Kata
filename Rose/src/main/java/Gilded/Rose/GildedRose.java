c0f7a7c554850c1a3788da03bda84f0338c08863
alexander_kif <alexander_kif@mail.ru>
2016-12-31 10:54:20 +0400

    new class GildedRoseSulfuras, @Override updateItem, switch
    Now Sulfuras updated in class GildedRoseSulfuras

--- Rose/src/main/java/Gilded/Rose/GildedRose.java
+++ Rose/src/main/java/Gilded/Rose/GildedRose.java
@@ -9,18 +9,24 @@ class GildedRose {
 
     public static void updateQuality() {
         for (int i = 0; i < items.size(); i++) {
-        	gildedRose = new GildedRose();
+        	switch (items.get(i).name) {
+			case "Sulfuras, Hand of Ragnaros":
+				gildedRose = new GildedRoseSulfuras();
+				break;
+
+			default:
+				gildedRose = new GildedRose();
+				break;
+			}
         	gildedRose.updateItem(items.get(i));
         }
     }
 
-	private void updateItem(Item item) {
+	public void updateItem(Item item) {
 		if (!item.name.equals("Aged Brie")
 		        && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 		    if (item.quality > 0) {
-		        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
 		            item.quality = item.quality - 1;
-		        }
 		    }
 		} else {
 		    if (item.quality < 50) {
@@ -42,17 +48,13 @@ class GildedRose {
 		    }
 		}
 
-		if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-		    item.sellIn = item.sellIn - 1;
-		}
+		item.sellIn = item.sellIn - 1;
 
 		if (item.sellIn < 0) {
 		    if (!item.name.equals("Aged Brie")) {
 		        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 		            if (item.quality > 0) {
-		                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
 		                    item.quality = item.quality - 1;
-		                }
 		            }
 		        } else {
 		            item.quality = item.quality - item.quality;
---
+++ Rose/src/main/java/Gilded/Rose/GildedRoseSulfuras.java
@@ -0,0 +1,10 @@
+package Gilded.Rose;
+
+public class GildedRoseSulfuras extends GildedRose {
+
+	@Override
+	public void updateItem(Item item)
+    {
+		//do nothing
+    }
+}
